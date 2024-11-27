d19bbbc8101f74edf9681e5f978c736c60198284
NgHungNam <nguyenhungnam2803@gmail.com>
2024-11-27 21:56:38 +0700

    5 bai tap

---
+++ exam.js
@@ -0,0 +1,122 @@
+
+
+
+// Bài 1
+
+// input
+let a = 100000; // a là lương 1 ngày
+let b = 25;     // b là số ngày làm
+ 
+// output
+let c = 0;      
+
+// progress
+c = a * b;       // c là tổng lương,  c = 100000 * 25 = 250000
+console.log('Tổng lương = ', c);
+
+
+
+
+
+
+
+
+
+
+// Bài 2
+
+// input
+let A = 5;            // A, B, C, D là 5 số thực
+let B = 10;
+let C = 15;
+let D = 20;
+let E = 25;
+
+// output
+let F = 0;           // F là trung bình của 5 số trên
+
+// progress
+F = (A + B + C + D + E) / 5;
+// F = 75 / 5 = 15
+console.log('Trung Bình = ', F);
+
+
+
+
+
+
+
+
+
+// Bài 3
+
+// input
+let d = 23500;   // d là giá USD hiện nay
+let e = 4;      // e là số USD
+
+// output
+let f = 0;     // f là số tiền sau quy đổi
+
+// progress
+f = d * e;      // f = 23500 * 4 = 94000
+console.log('số tiền VND = ', f);
+f = 94000;
+
+
+
+
+
+
+
+
+
+// Bài 4 
+
+//input
+let I = 3;     // I là chiều rộng
+let V = 5;     // V là chiều rộng
+
+// output
+let X = 0;    // X là chu vi
+let Z = 0;    // Z là diện tích
+
+// progress
+X = (I + V) * 2 ;       // X = 8 * 2 = 16
+Z = I * V;               // Z = 3 * 5 = 15
+console.log('Chu Vi = ', X);
+console.log('Diện Tích = ', Z);
+
+X = 16;
+Z = 15;
+
+
+
+
+
+
+
+
+
+// Bài 5 
+
+// input
+let so = 22;
+
+// ouput
+let tongKySo = 0;
+
+// progress
+let hangChuc = so / 10;
+let hangDonVi = so % 10;
+console.log('Tổng ký số =', tongKySo);
+
+tongKySo = 4;
+
+
+
+
+
+
+
+
+
